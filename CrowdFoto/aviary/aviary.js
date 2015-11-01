59ab0a1578a04d8375eb0e3b34e4f0c94f97edd9
MichaelGofron <gofronm@yahoo.com>
2015-11-01 13:13:45 -0600

    (feat) Store newly routed pages

---
+++ CrowdFoto/aviary/aviary.js
@@ -0,0 +1,16 @@
+// var fs = require('fs');
+// var featherEditor = new Aviary.Feather({
+//     apiKey: '485d12df-f741-403b-8413-aac1665c63ef',
+//     onSave: function(imageID, newURL) {
+//         var img = document.getElementById(imageID);
+//         img.src = newURL;
+//     }
+// });
+
+// function launchEditor(id, src) {
+//     featherEditor.launch({
+//         image: id,
+//         url: src
+//     });
+//     return false;
+// }
